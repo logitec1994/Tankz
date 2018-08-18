f2c5be1be3ee7f5df5f65517660459dc0d749034
logitec1994 <123654978a@gmail.com>
2018-08-18 12:28:03 +0300

    Commit for diff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,92 @@
 #include <iostream>
+#include <conio.h>
 
-int main()
+const int W = 40;
+const int H = 20;
+bool is_game = true;
+int plX, plY;
+int enX, enY;
+int bX, bY;
+
+void setup()
 {
-    std::cout << "Hello, World!" << std::endl;
+    is_game = true;
+    plX = W/2;
+    plY = H/2;
+    enX = rand() % W;
+    enY = rand() % H;
+}
+
+void draw()
+{
+    system("cls");
+    for (int i = 0; i < H; ++i)
+    {
+        for (int j = 0; j < W; ++j)
+        {
+            if (i == 0 || i == H-1)
+            {
+                std::cout << "#";
+            } else if (j == 0 || j == W-1)
+            {
+                std::cout << "#";
+            } else if (i == plY && j == plX)
+            {
+                std::cout << "F";
+            } else if (i == bY && j == bX)
+            {
+                std::cout << "b";
+            } else if (i == enY && j == enX)
+            {
+                std::cout << "T";
+            } else
+            {
+                std::cout << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+void logic()
+{
+    char c = getche();
+    if (plX == 0 || plX == W-1)
+    {
+        is_game = false;
+    }
+    if (plY == 0 || plY == H-1)
+    {
+        is_game = false;
+    }
+    if (c == 'w')
+    {
+        plY--;
+    }
+    if (c == 's')
+    {
+        plY++;
+    }
+    if (c == 'd')
+    {
+        plX++;
+    }
+    if (c == 'a')
+    {
+        plX--;
+    }
+
+}
+
+int main(int argc, char const *argv[])
+{
+
+    setup();
+
+    while(is_game)
+    {
+        draw();
+        logic();
+    }
     return 0;
 }
